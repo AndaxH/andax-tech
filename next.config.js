8929c53c7c32868894a7e64a3ecf3374ccd1f0df
Andrew Hammond <andaxhambert@gmail.com>
2024-01-26 12:06:37 +1100

    Foo page and redirects in next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,29 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.jsx'
 })
-module.exports = withNextra()
+
+const nextraExports = withNextra()
+const nextraExportsPlus = {
+  ...nextraExports,
+  async redirects() {
+    return [
+      {
+        source: '/tea',
+        destination: '/tea/index.html',
+        permanent: true
+      },
+      {
+        source: '/zoom',
+        destination: '/zoom-captions/index.html',
+        permanent: true
+      },
+      {
+        source: '/zoom-captions',
+        destination: '/zoom-captions/index.html',
+        permanent: true
+      }
+    ]
+  }
+}
+
+module.exports = nextraExportsPlus
