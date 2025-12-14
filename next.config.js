485c925840d08098f6669815e34ad7204c1696f5
Andax <andaxhambert@gmail.com>
2025-12-14 23:03:35 +1100

    Webpack for build too and remove unused key

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const nextraExports = withNextra()
 const nextraExportsPlus = {
   ...nextraExports,
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
